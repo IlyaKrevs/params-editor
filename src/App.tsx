d0d40edd468556933dc8be3bca789cccba21700a
Krevs <m-u-r-z-a@mail.ru>
2023-12-28 18:57:37 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+interface Param {
+  id: number;
+  name: string;
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+}
+
+
+
 function App() {
+
+  let myParamArr: Param[] = [
+    {
+      "id": 1,
+      "name": "Назначение"
+    },
+    {
+      "id": 2,
+      "name": "Длина"
+    },
+    {
+      "id": 3,
+      "name": "Check"
+    },
+  ];
+
+  let myModel: Model = {
+    "paramValues": [
+      {
+        "paramId": 1,
+        "value": "повседневное"
+      },
+      {
+        "paramId": 2,
+        "value": "check"
+      },
+      {
+        "paramId": 3,
+        "value": "проверка"
+      },
+    ]
+  };
+
+
+  let [modelState, setModelState] = useState(myModel.paramValues)
+
+
+  function setModelStateCALLBACK(id: number | undefined, value: string) {
+    if (typeof id !== undefined) {
+      setModelState(prevState => prevState.map(item => {
+        return item.paramId === id ? { ...item, value } : item
+      }))
+    }
+  }
+
+
+  function createInputsList(modelObj: ParamValue[], nameArr: Param[]) {
+    let myModelValueArr: ParamValue[] = modelObj;
+
+    return myModelValueArr.map((item: ParamValue) => {
+      let paramObj = nameArr.find((temp) => temp.id == item.paramId)
+
+
+      return (
+        <MyInput paramObj={paramObj} initValue={item} callBack={setModelStateCALLBACK} />
+      )
+    })
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        {createInputsList(modelState, myParamArr)}
+        <button onClick={() => console.log(modelState)}>Console log MODEL</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+interface InputProps {
+  paramObj?: Param,
+  initValue: ParamValue,
+  callBack: (id: number | undefined, value: string) => void
+}
+
+function MyInput({ paramObj, initValue, callBack }: InputProps) {
+
+
+  return (
+    <label className='inputContainer'>
+      {paramObj?.name}
+      <input type="text" value={initValue.value} onChange={(e) => callBack(paramObj?.id, e.target.value)} />
+    </label>
+  )
+}
